ac01347155791e77c611675587fe7b36750476b1
Phuc Quang Tran <tranquangphuc@outlook.com>
2021-02-05 22:05:01 +0700

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mộng Mơ's Blog`,
+    description: `A Java Developer's Life`,
+    author: `@tranquangphuc`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
